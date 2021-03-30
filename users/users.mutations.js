03e4f87e72d9bafcd63c925468f9d8d0a3c3ea83
LunchTime-github <hiyoon9@gmail.com>
2021-03-30 16:43:58 +0900

    #4.5 login part Two and Refactor

--- users/users.mutations.js
+++ users/users.mutations.js
@@ -1,3 +1,4 @@
+import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 import client from "../client";
 
@@ -39,6 +40,32 @@ export default {
       } catch (e) {
         return e;
       }
+    },
+    login: async (_, { username, password }) => {
+      // find user with atgs.username
+      const user = await client.user.findFirst({
+        where: { username },
+      });
+      if (!user) {
+        return {
+          ok: false,
+          error: "User not found.",
+        };
+      }
+      // check password width args.password
+      const passwordOk = await bcrypt.compare(password, user.password);
+      if (!passwordOk) {
+        return {
+          ok: false,
+          error: "Incorrect password.",
+        };
+      }
+      // issue a token and send it to the user
+      const token = jwt.sign({ id: user.id }, process.env.SECRET_KEY);
+      return {
+        ok: true,
+        token,
+      };
     },
   },
 };
