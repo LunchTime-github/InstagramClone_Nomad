1f668e6258f80a80c32ea45631e8f2437c9c7596
LunchTime-github <hiyoon9@gmail.com>
2021-04-02 17:19:53 +0900

    #4.24 Followers Pagination part Two

--- users/seeFollowers/seeFollowers.resolvers.js
+++ users/seeFollowers/seeFollowers.resolvers.js
@@ -3,16 +3,41 @@ import client from "../../client";
 export default {
   Query: {
     seeFollowers: async (_, { username, page }) => {
+      // check followers
+      const ok = await client.user.findUnique({
+        where: { username },
+        select: { id: true },
+      });
+      console.log(ok);
+      if (!ok) {
+        return {
+          ok: false,
+          error: "User not found",
+        };
+      }
       // find followers
+      const take = 2;
       const followers = await client.user
         .findUnique({ where: { username } })
         .followers({
-          take: 4,
-          skip: Math.max(0, page - 1) * 4, // skip 은 반드시 양수여야만 함
+          take,
+          skip: Math.max(0, page - 1) * take, // skip 은 반드시 양수여야만 함
         });
+
+      const totalFollowers = await client.user.count({
+        where: {
+          following: {
+            some: {
+              username,
+            },
+          },
+        },
+      });
+
       return {
         ok: true,
         followers,
+        totalPages: Math.ceil(totalFollowers / take),
       };
     },
   },
