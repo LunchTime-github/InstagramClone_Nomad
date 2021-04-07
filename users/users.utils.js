19a7c615bdd9d8ea02f2e527807a29d28fedb9a9
LunchTime-github <hiyoon9@gmail.com>
2021-04-07 18:18:15 +0900

    #6.18 protectedResolver Refactor

--- users/users.utils.js
+++ users/users.utils.js
@@ -17,12 +17,22 @@ export const getUser = async (token) => {
   }
 };
 
-export const protectedResolver = (ourResolver) => (root, args, context, info) => {
+export const protectedResolver = (ourResolver) => (
+  root,
+  args,
+  context,
+  info
+) => {
   if (!context.loggedInUser) {
-    return {
-      ok: false,
-      error: "Please log in to perform this action.",
-    };
+    const query = info.operation.operation === "query";
+    if (query) {
+      return null;
+    } else {
+      return {
+        ok: false,
+        error: "Please log in to perform this action.",
+      };
+    }
   }
   return ourResolver(root, args, context, info);
 };
