3330717519f1d58d43243c8995e58d0561bba637
LunchTime-github <hiyoon9@gmail.com>
2021-04-20 17:10:29 +0900

    #7.10 Filtering Subscriptions

--- messages/roomUpdates/roomUpdates.resolvers.js
+++ messages/roomUpdates/roomUpdates.resolvers.js
@@ -1,10 +1,27 @@
+import { withFilter } from "graphql-subscriptions";
+import client from "../../client";
 import { NEW_MESSAGE } from "../../constants";
 import pubsub from "../../pubsub";
 
 export default {
   Subscription: {
     roomUpdates: {
-      subscribe: () => pubsub.asyncIterator(NEW_MESSAGE),
+      subscribe: async (root, args, context, info) => {
+        const room = await client.room.findUnique({
+          where: { id: args.id },
+          select: { id: true },
+        });
+
+        if (!room) {
+          throw new Error("You shall not see this");
+        }
+        return withFilter(
+          () => pubsub.asyncIterator(NEW_MESSAGE),
+          ({ roomUpdates: { roomId } }, { id }) => {
+            return roomId === id;
+          }
+        )(root, args, context, info);
+      },
     },
   },
 };
